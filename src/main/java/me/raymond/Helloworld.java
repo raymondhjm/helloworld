09134550a27649c7c08e5a0a3fc45277035db6cc
raymondhjm <raymond.hjm@gmail.com>
2016-05-03 16:09:19 +0800

    update helloworld

--- src/main/java/me/raymond/Helloworld.java
+++ src/main/java/me/raymond/Helloworld.java
@@ -4,6 +4,5 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world2!");
-        System.out.println("Hello world3!");
     }
 }
