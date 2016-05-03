572bedc09c453c4effd7a36e1841098b4c9d62d8
raymondhjm <raymond.hjm@gmail.com>
2016-05-03 16:02:40 +0800

    update helloworld 2

--- src/main/java/me/raymond/Helloworld.java
+++ src/main/java/me/raymond/Helloworld.java
@@ -3,5 +3,7 @@ package me.raymond;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world2!");
+        System.out.println("Hello world3!");
     }
 }
