113878668ccdb37b8fd947f72e09c5ddf1f64777
raymondhjm <raymond.hjm@gmail.com>
2016-05-03 16:17:05 +0800

    Squashed commit of the following:
    commit 508ac029c12fe4be632b0d9594a138e3e8a749e5
    Author: raymondhjm <raymond.hjm@gmail.com>
    Date:   Tue May 3 16:15:55 2016 +0800
    
        update helloworld10
    
    commit 32ffefaa6b42a26c2bd5c47e88a6b75cdae7e7db
    Author: raymondhjm <raymond.hjm@gmail.com>
    Date:   Tue May 3 16:15:45 2016 +0800
    
        update helloworld9
    
    commit 3075bcd3748670d1b108b9cb46815b8f1a2eb962
    Author: raymondhjm <raymond.hjm@gmail.com>
    Date:   Tue May 3 16:15:36 2016 +0800
    
        update helloworld8

--- src/main/java/me/raymond/Helloworld.java
+++ src/main/java/me/raymond/Helloworld.java
@@ -9,5 +9,8 @@ public class Helloworld {
         System.out.println("Hello world5!");
         System.out.println("Hello world6!");
         System.out.println("Hello world7!");
+        System.out.println("Hello world8!");
+        System.out.println("Hello world9!");
+        System.out.println("Hello world10!");
     }
 }
